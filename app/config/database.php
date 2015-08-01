99dc81ddb09a06e75eda3ffb709aaa44db424aa4
Udeme Samuel <udemesamuel256@gmail.com>
2015-08-01 07:29:58 +0100

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -45,8 +45,7 @@ return array(
 	*/
 	
 	
-	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
+	$url = parse_url(getenv('CLEARDB_DATABASE_URL'));
 	$host = $url["host"];
 	$username = $url["user"];
 	$password = $url["pass"];
